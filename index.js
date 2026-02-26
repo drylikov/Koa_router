bdd74ed1aee5cafc4c0f22e26a764c2e493d7549
drylikov <denis.rylikov@protonmail.com>
2026-02-26 12:27:23 +0300

    Koa_router
    Router middleware for koa. Provides RESTful resource routing.

---
+++ index.js
@@ -0,0 +1,9 @@
+
+/**
+ * RESTful resource routing middleware for koa.
+ *
+ * @author Denis Rylikov <denis.rylikov@protonmail.com>
+ * @link https://github.com/drylikov/koa-router
+ */
+
+module.exports = require('./lib/router');
---
+++ test/index.js
@@ -0,0 +1,16 @@
+
+/**
+ * Module tests
+ */
+
+var koa = require('koa')
+  , should = require('should');
+
+describe('module', function() {
+  it('should expose Router', function(done) {
+    var Router = require('..');
+    should.exist(Router);
+    Router.should.be.a('function');
+    done();
+  });
+});
